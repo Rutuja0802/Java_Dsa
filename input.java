f7514b8b04aad7727f59d61035fa3fe5c7278854
Rutuja Kishor Pathare <91882504+Rutuja0802@users.noreply.github.com>
2022-06-16 19:43:57 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class input{
+    public static Scanner scn=new Scanner(System.in);
+    public static void main(String[] args){
+
+        //input for int
+        // int a = scn.nextInt();
+        // System.out.println("value of a is" + a);
+
+
+        //input for long
+        // long a = scn.nextLong();
+        // System.out.println("value of a is" + a);
+
+        //input for String
+        string str= scn.next();
+        System.out.println("value of a is" + str);
+    }
+}
